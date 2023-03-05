a9f1646314067f72b903c3bcb716970e4cbea055
Leandrocvida <leandrocvida@gmail.com>
2023-03-05 20:37:13 -0300

    endpoints

---
+++ src/modules/cities/city.repository.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@nestjs/common';
+import { Repository, DataSource } from 'typeorm';
+import { CreateCityDto } from './dto/create-city.dto';
+import { CityEntity } from './entities/city.entity';
+import { InjectDataSource } from '@nestjs/typeorm';
+
+@Injectable()
+export class CityRepository extends Repository<CityEntity> {
+  constructor(@InjectDataSource() dataSource: DataSource) {
+    super(CityEntity, dataSource.createEntityManager());
+  }
+  async getById(id: number): Promise<CityEntity> {
+    return this.findOne({ where: { id } });
+  }
+  async deleteCity(city: CityEntity): Promise<boolean> {
+    const cityDeleted = await this.delete(city);
+
+    if (cityDeleted) return true;
+    return false;
+  }
+
+  async updateCity(city: CityEntity): Promise<CityEntity> {
+    const cityUpdate = await this.save(city);
+    return cityUpdate;
+  }
+  async getByAll(): Promise<CityEntity[]> {
+    return this.find();
+  }
+
+  async createCity(newCity: CreateCityDto): Promise<void> {
+    const city = new CityEntity();
+    city.state_id = newCity.state_id;
+    city.name = newCity.name;
+
+    await this.save(city);
+  }
+}
